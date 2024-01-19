ea2a14a8d7bfd49397ef2236da39f1f940fecf62
stossy11 <69031796+stossy11@users.noreply.github.com>
2024-01-19 17:59:43 +1100

    Add files via upload

--- restore.sh
+++ restore.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -e
+#!/bin/bash
 
 realpath() {
     canonicalize_path "$(resolve_symlinks "$1")"
@@ -68,8 +68,27 @@ _canonicalize_file_path() {
     (cd "$dir" 2>/dev/null && printf '%s/%s\n' "$(pwd -P)" "$file")
 }
 
+if [ "x`uname`" != "xDarwin" ] ; then
+	echo "Only Mac OS X is supported."
+	exit 1
+fi
+
+if [ "x`uname -m`" = "xi386" ] ; then
+	echo "Only 64-bit Macs are supported."
+	exit 1
+fi
+
+get_key() {
+	cat "$1" | grep -A1 "<key>$2</key>" | tail -1 | cut -d '>' -f 2 | cut -d '<' -f 1
+}
+
 usage() {
-	echo "Usage: $1 <input.ipsw> [jailbreak | reset]"
+	echo "Usage: $1 <input.ipsw>"
+}
+
+_exit() {
+	rm -f Restore.plist *.shsh2
+	exit 1
 }
 
 if [ "x$1" = "x" ]; then
@@ -77,20 +96,59 @@ if [ "x$1" = "x" ]; then
 	exit 1
 fi
 
-if [ "x$2" != "x" ] && [ "$2" != "jailbreak" ] && [ "$2" != "reset" ] ;then
-	usage "$0"
-	exit 1
-fi
+cd "$(dirname "$0")"
 
-if [ ! -f "$1" ] ; then
+trap _exit SIGINT SIGTERM
+
+if [ ! -f "`realpath "$1"`" ] ; then
 	echo "Can't read IPSW file: $1"
 	exit 1
 fi
 
-cd "$(dirname "$0")"
-for i in scripts/*.sh; do
-	bash -e $i "`realpath "$1"`" "$2"
-done
+unzip -p "`realpath "$1"`" Restore.plist > Restore.plist 2>/dev/null
+if [ ! -s Restore.plist ] ; then
+	echo "Not an IPSW file: $1"
+	rm -f Restore.plist
+	exit 1
+fi
 
-./restore_only.sh $iname 
-exit $?
+ecid=`ioreg -l -w0 | grep "USB Serial Number" | grep -m 1 "iBoot-574.4" | sed 's/^.*ECID://' | sed 's/ .*//'`
+if [ "x$ecid" = x ]; then
+	echo "Can't connect to your iPhone. It needs to be in DFU mode."
+	rm -f Restore.plist
+	exit 1
+fi
+
+set -e
+
+model=`get_key Restore.plist ProductType`
+vers=`get_key Restore.plist ProductVersion`
+rm -f Restore.plist
+rm -f *.shsh2
+mkdir -p shsh
+ecidf="shsh/$(printf "%d" 0x$ecid)-${model}-DG.shsh"
+if [ ! -f "$ecidf" ]; then
+	echo Downloading SHSH for ECID 0x$ecid
+	./tools/tsschecker -e 0x$ecid -d $model -m BuildManifest.plist -s
+	mv *.shsh2 "$ecidf"
+else
+	echo Using cached SHSH for ECID 0x$ecid
+fi
+rm -rf "`realpath "$1" | sed 's/\.ipsw$//'`"
+killall iTunes iTunesHelper >/dev/null 2>&1 || true
+killall -STOP AMPDeviceDiscoveryAgent >/dev/null 2>&1 || true
+cd tools/ipwndfu
+arch -x86_64 /usr/local/bin/python ./ipwndfu -p
+cd ../..
+echo
+echo 'IMPORTANT: an "FDR" error is normal, ignore it'
+echo
+set +e
+./tools/idevicerestore -y -e -w "`realpath "$1"`"
+ex=$?
+rm -rf "`realpath "$1" | sed 's/\.ipsw$//'`"
+killall -CONT AMPDeviceDiscoveryAgent >/dev/null 2>&1 || true
+if [ $ex != 0 ]; then
+exit $ex
+fi
+echo "Finished! Enjoy iOS $vers" 
